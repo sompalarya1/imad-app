46cf16481110e2deda5a6c7c3c5b7fec2d9a164e
sompalarya1 <31383339+sompalarya1@users.noreply.github.com>
2017-09-05 21:52:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,8 +61,10 @@ marginLeft=0;
 function moveRight(){
     marginLeft = marginLeft + 5;
     img.style.marginLeft + 'px';
-}
-
+};
+img.onclick = function(){
+    var interval =setInterval(moveRight(),50);
+};
 
 
 
