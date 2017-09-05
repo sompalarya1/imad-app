94803029b7ebbf94881c4459c75c033071ba192a
sompalarya1 <31383339+sompalarya1@users.noreply.github.com>
2017-09-05 12:33:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res)){
+
+app.get('/article-one', function(req,res)){
  res.send('Article one  is requested');
-};
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
